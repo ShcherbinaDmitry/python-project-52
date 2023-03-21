80ee093ed633325c79b668f792c82b5bec4a3551
ShcherbinaDmitry <shcherbinadmitry@gmail.com>
2023-03-21 15:33:48 +0300

    feat: project deploy setup

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
+PORT ?= 8000
 start:
+		poetry run gunicorn -w 5 -b 0.0.0.0:$(PORT) task_manager.wsgi
+
+dev:
 		python3 manage.py runserver
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-=$%#sbi=yf#v#rnr&$!*xd=-z_cpi!x#0zk!+@pwsdpyl4fo)3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["webserver", '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
