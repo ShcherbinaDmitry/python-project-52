c10536133d6646231073e9e43bcb92e84f9d3bf0
ShcherbinaDmitry <shcherbinadmitry@gmail.com>
2024-02-25 20:49:56 +0300

    feat: add statuses app

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class Status(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name=_("Name"),
+        unique=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("Creation date"),
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Status")
+        verbose_name_plural = _("Statuses")
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import ListView
+from task_manager.mixins import CustomLoginMixin, DeleteProtectionMixin
+from task_manager.statuses.models import Status
+from task_manager.views import (
+    CustomCreateView,
+    CustomDeleteView,
+    CustomUpdateView,
+)
 
-# Create your views here.
+from .forms import StatusForm
+
+
+class StatusesListView(CustomLoginMixin, ListView):
+    template_name = 'statuses/index.html'
+    model = Status
+    context_object_name = 'statuses'
+
+
+class StatusCreateView(CustomLoginMixin, CustomCreateView):
+    model = Status
+    form_class = StatusForm
+    success_url = reverse_lazy('statuses')
+    success_message = _('Status successfully created')
+    extra_context = {
+        'header': _('Create status'),
+        'button_text': _('Create'),
+    }
+
+
+class StatusUpdateView(CustomLoginMixin, CustomUpdateView):
+    model = Status
+    form_class = StatusForm
+    success_url = reverse_lazy('statuses')
+    success_message = _('Status is successfully updated')
+    extra_context = {
+        'header': _('Update user'),
+        'button_text': _('Update'),
+    }
+
+
+class StatusDeleteView(
+        CustomLoginMixin, DeleteProtectionMixin,
+        CustomDeleteView):
+    model = Status
+    success_url = reverse_lazy('statuses')
+    success_message = _('Status successfully deleted')
+    protected_message = _("Cannot delete a status because it is in use")
+    protected_url = reverse_lazy('statuses')
+    extra_context = {
+        'header': _('Delete status'),
+        'button_text': _('Yes, delete'),
+    }
