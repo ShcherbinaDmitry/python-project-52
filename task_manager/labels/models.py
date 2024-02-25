e762ef62de37f871e7635025a7d41c0020971e75
ShcherbinaDmitry <shcherbinadmitry@gmail.com>
2024-02-25 20:47:10 +0300

    feat: add labels app

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,3 +1,22 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Label(models.Model):
+    name = models.CharField(
+        verbose_name=_("Name"),
+        max_length=150,
+        unique=True
+    )
+    created_at = models.DateTimeField(
+        verbose_name=_("Creation date"),
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Label")
+        verbose_name_plural = _("Labels")
--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 # Create your views here.
+from django.views.generic import ListView
+from task_manager.labels.forms import LabelForm
+from task_manager.labels.models import Label
+from task_manager.mixins import CustomLoginMixin, DeleteProtectionMixin
+from task_manager.views import (
+    CustomCreateView,
+    CustomDeleteView,
+    CustomUpdateView,
+)
+
+
+class LabelsListView(CustomLoginMixin, ListView):
+    template_name = 'labels/index.html'
+    model = Label
+    context_object_name = 'labels'
+
+
+class LabelCreateView(CustomLoginMixin, CustomCreateView):
+    model = Label
+    form_class = LabelForm
+    success_message = _("Label is successfully created")
+    success_url = reverse_lazy('labels')
+    extra_context = {
+        'header': _('Create label'),
+        'button_text': _('Create'),
+    }
+
+
+class LabelUpdateView(CustomLoginMixin, CustomUpdateView):
+    model = Label
+    form_class = LabelForm
+    success_url = reverse_lazy('labels')
+    success_message = _("Label is successfully updated")
+    extra_context = {
+        'header': _("Label update"),
+        'button_text': _("Update")
+    }
+
+
+class LabelDeleteView(
+        DeleteProtectionMixin, CustomDeleteView):
+    model = Label
+    success_message = _("Label is successfully deleted")
+    success_url = reverse_lazy('labels')
+    protected_message = _("Cannot delete a label because it is in use")
+    protected_url = reverse_lazy('labels')
+    extra_context = {
+        'header': _("Delete label"),
+        'button_text': _("Yes, delete")
+    }
