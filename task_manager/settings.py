a46702f5971b770c65fb9971abc01c1a5daf5083
ShcherbinaDmitry <shcherbinadmitry@gmail.com>
2024-02-28 17:19:25 +0300

    feat: update i18n

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -126,15 +126,19 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "ru"
+LANGUAGE_CODE = "ru-RU"
 
 TIME_ZONE = "UTC"
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
-LOCALE_PATHS = ["/locale"]
+# LOCALE_URL = "/locale/"
+
+LOCALE_PATHS = [BASE_DIR / "locale"]
 
 
 # Static files (CSS, JavaScript, Images)
