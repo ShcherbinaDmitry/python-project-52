5c2839bb0ca9e8cad44f79e40e5e30a51e631aeb
ShcherbinaDmitry <shcherbinadmitry@gmail.com>
2024-02-28 15:19:54 +0300

    feat: add app views

---
+++ task_manager/views.py
@@ -0,0 +1,48 @@
+from django.contrib import messages
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    TemplateView,
+    UpdateView,
+)
+
+from .mixins import ObjectContextMixin
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+
+class UserLoginView(SuccessMessageMixin, LoginView):
+    template_name = "form.html"
+    next_page = reverse_lazy("index")
+    success_message = _("You are logged in")
+    extra_context = {
+        "header": _("Login"),
+        "button_text": _("Login"),
+    }
+
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy("index")
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.info(request, _("You are logged out."))
+        return super().dispatch(request, *args, **kwargs)
+
+
+class CustomCreateView(SuccessMessageMixin, CreateView):
+    template_name = "form.html"
+
+
+class CustomUpdateView(SuccessMessageMixin, UpdateView):
+    template_name = "form.html"
+
+
+class CustomDeleteView(
+        ObjectContextMixin, SuccessMessageMixin, DeleteView):
+    template_name = "delete_form.html"
