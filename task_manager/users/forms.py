64b11720bd3472b1945f3f8d1fb8f48b7418d840
ShcherbinaDmitry <shcherbinadmitry@gmail.com>
2024-02-29 13:04:07 +0300

    refactor: user forms

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -14,35 +14,45 @@ def get_widget(placeholder):
     )
 
 
-class BasicUserForm:
-    password1 = forms.CharField(
-        label=_("Password"),
-        widget=get_widget("Password"),
-        help_text=_("Your password must be at least 3 characters long"),
+password1 = forms.CharField(
+    label=_("Password"),
+    widget=get_widget("Password"),
+    help_text=_("Your password must be at least 3 characters long"),
+)
+password2 = forms.CharField(
+    label=_("Password confirmation"),
+    widget=get_widget("Password confirmation"),
+    help_text=_("To confirm, please enter your password again"),
+)
+
+
+class UserMeta:
+    model = User
+    fields = (
+        "first_name",
+        "last_name",
+        "username",
+        "password1",
+        "password2",
     )
-    password2 = forms.CharField(
-        label=_("Password confirmation"),
-        widget=get_widget("Password confirmation"),
-        help_text=_("To confirm, please enter your password again"),
-    )
-
-    class Meta:
-        model = User
-        fields = (
-            "first_name",
-            "last_name",
-            "username",
-            "password1",
-            "password2",
-        )
 
 
-class RegisterUserForm(UserCreationForm, BasicUserForm):
-    pass
+class RegisterUserForm(UserCreationForm):
+    password1 = password1
+    password2 = password2
+
+    class Meta(UserMeta):
+        pass
 
 
-class UpdateUserForm(UserChangeForm, BasicUserForm):
+class UpdateUserForm(UserChangeForm):
     password = None
+
+    password1 = password1
+    password2 = password2
+
+    class Meta(UserMeta):
+        pass
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
